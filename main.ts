68e50a531e7d9fa0853fd85281a2a0d3470ccf18
Alex-Web940 <110633160+Alex-Web940@users.noreply.github.com>
2022-08-04 15:18:17 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,61 @@
+let Numero = 0
+input.onButtonPressed(Button.A, function () {
+    Numero = randint(1, 6)
+    basic.showNumber(Numero)
+})
+input.onGesture(Gesture.Shake, function () {
+    Numero = randint(1, 6)
+    if (Numero == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Numero == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Numero == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Numero == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Numero == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Numero = randint(1, 6)
+    basic.showNumber(Numero)
+})
